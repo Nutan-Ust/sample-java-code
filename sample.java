2691ecece3fa9541180640d7e04fdb8dfb277657
Anushiya Murukaiah <56147275+AnushiyaM11@users.noreply.github.com>
2024-08-28 20:23:33 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,46 +1,61 @@
-// BAD EXAMPLE: Violates multiple Google Java Style Guide guidelines
+import java.util.*;import java.io.*;
  
-import java.util.*;import java.io.File;import java.util.function.*;
+class badCode extends Object{
+public static void main(String args[]) throws Exception {
+System.out.println("This code violates many style guidelines!");
  
-public class badExampleClass { // Bad class name convention
-    public static final int some_CONSTANT = 10; // Bad constant naming convention
+// Violating naming conventions
+int THIS_IS_not_A_CONSTANT = 42;
+String UpperCamelCaseForMethod() { return "Hello"; }
  
-    private List<String> l = new ArrayList<String>(); // Non-descriptive variable name
- 
-    public static void main(String args[]) { // String array should be final and named 'args'
-        badExampleClass bec = new badExampleClass(); // Class name not camel case
-        bec.add("exampleString");bec.add(null); bec.add(""); // No space after semicolon, nulls and empty strings should be handled
-        bec.doSomething(100); // Magic number, use constant instead
-        bec.doSomething(some_CONSTANT);
-    }
- 
-    public void add(String s) {
-        l.add(s); // Should check for null or empty string
-    }
- 
-    public void doSomething(int n) {
-        if (n == 100) { // Magic number, use a named constant
-            System.out.println("Doing something"); // Avoid using System.out.println for logging
-        } else if (n == some_CONSTANT) {
-            System.out.println("Doing something else"); // Avoid using System.out.println for logging
-        } else {
-            System.out.println("Doing nothing"); // Avoid using System.out.println for logging
-        }
-    }
- 
-    public void veryLongMethodNameThatShouldBeShorter() { // Method name is too long
-        // Code that is too long and complex
-        try {
-            File file = new File("path/to/file"); // Hardcoded file path
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
-            }
-            scanner.close();
-        } catch (Exception e) { // Catching generic exception, should be specific
-            e.printStackTrace(); // Using printStackTrace, should use logger
-        }
-    }
+// Violating brace style and indentation
+if (THIS_IS_not_A_CONSTANT > 0)
+{
+System.out.println("Positive");
 }
-
-has context menu
+else {
+    System.out.println("Non-positive");}
+ 
+// Violating column limit and line wrapping
+String veryLongString = "This is an extremely long string that goes way beyond the recommended 100 character limit and doesn't use proper line wrapping techniques";
+ 
+// Violating whitespace rules
+int[]array={1,2,3,4,5};
+ 
+// Violating comment style
+/* This is a multi-line comment
+that doesn't follow proper
+indentation rules */
+ 
+// Violating switch statement formatting
+int x = 1;
+switch(x){
+case 1: System.out.println("One");break;
+case 2:
+System.out.println("Two");
+break;
+default:System.out.println("Other");}
+ 
+// Violating try-catch block formatting and exception handling
+try{
+File file=new File("nonexistent.txt");
+Scanner scanner=new Scanner(file);
+}catch(FileNotFoundException e){}
+ 
+// Violating method naming and parameter conventions
+public static void BAD_METHOD_NAME(final int X, int Y, int z) {
+    // Method body
+}
+ 
+// Violating enum formatting
+enum Color{RED,GREEN,BLUE};
+ 
+// Violating variable declarations
+int a,b,c=1,d=2;
+ 
+// Violating naming for type parameters
+class GenericClass<t> {
+    // Class body
+}
+ 
+}
